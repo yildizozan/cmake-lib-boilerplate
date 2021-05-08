e26e9e9122a6f270cff20788447e320017796705
Ozan Yıldız <developer@yildizozan.com>
2021-05-08 15:25:31 +0300

    Fundamental

---
+++ include/Calculator.h
@@ -0,0 +1,15 @@
+//
+// Created by yildizozan on 8.05.2021.
+//
+
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+
+class Calculator {
+public:
+    int sum(int a, int b) const;
+};
+
+
+#endif //CALCULATOR_H
---
+++ source/Calculator.cpp
@@ -0,0 +1,9 @@
+//
+// Created by yildizozan on 8.05.2021.
+//
+
+#include "Calculator.h"
+
+int Calculator::sum(int a, int b) const {
+    return a + b;
+}
